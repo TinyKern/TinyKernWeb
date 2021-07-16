f65918fc4835ba075a1e55bf72613f609a9d2b84
Owen Boreham <bobrossrtx@gmail.com>
2021-07-16 21:30:29 +0100

    initial commit

---
+++ TinyKernel/Markdown.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Markdig;
+
+namespace TinyKernel
+{
+  public static class Markdown
+  {
+    public static string Parse(string markdown)
+    {
+      if (string.IsNullOrEmpty(markdown))
+        return "";
+
+      var pipeline = new MarkdownPipelineBuilder()
+        .UseAdvancedExtensions()
+        .Build();
+
+      return Markdig.Markdown.ToHtml(markdown, pipeline);
+    }
+  }
+}
